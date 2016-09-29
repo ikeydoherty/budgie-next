7fb71e90bb63a750070294c1b4e4fb52c51fcb34
Ikey Doherty <ikey@solus-project.com>
2016-09-29 17:59:39 +0100

    Initial copy/pasta of budgie-panel
    Signed-off-by: Ikey Doherty <ikey@solus-project.com>

--- README.md
+++ README.md
@@ -7,7 +7,13 @@ I swears it.
 License
 -------
 
-This project is available under the terms of the `GPL-2.0` license.
+The shared libraries are available under the terms of the LGPL-2.1,
+allowing developers to link against the API without any issue, and
+to use all exposed APIs without affecting their project license.
+
+The remainder of the project (i.e. installed binaries) is available
+under the terms of the GPL 2.0 license. This is clarified in the headers
+of each source file.
 
 
 Authors
---
+++ common.mk
@@ -0,0 +1,16 @@
+AM_CFLAGS = -fstack-protector -Wall -pedantic \
+        -Wstrict-prototypes -Wundef -fno-common \
+        -Werror-implicit-function-declaration \
+        -Wformat -Wformat-security -Werror=format-security \
+        -Wno-conversion -Wunused-variable -Wunreachable-code \
+        -Wall -W -D_FORTIFY_SOURCE=2 \
+        -std=c11 -Wno-error
+
+AM_CPPFLAGS = \
+	-I $(top_srcdir)/src
+
+GTK_VERSIONING_FLAGS =						\
+        -DGDK_VERSION_MAX_ALLOWED=GDK_VERSION_3_20              \
+        -DGDK_VERSION_MIN_REQUIRED=GDK_VERSION_3_20             \
+        -DGLIB_VERSION_MAX_ALLOWED=GLIB_VERSION_2_48            \
+        -DGLIB_VERSION_MIN_REQUIRED=GLIB_VERSION_2_48
