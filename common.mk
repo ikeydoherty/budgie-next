6b6416476ffc686760eb7677995f30074c98c3e1
Ikey Doherty <ikey@solus-project.com>
2016-10-22 05:47:44 +0100

    Ensure we include UI
    Signed-off-by: Ikey Doherty <ikey@solus-project.com>

--- common.mk
+++ common.mk
@@ -8,7 +8,8 @@ AM_CFLAGS = -fstack-protector -Wall -pedantic \
 
 AM_CPPFLAGS = \
 	-I $(top_srcdir)/src \
-	-I $(top_srcdir)/src/libsolus
+	-I $(top_srcdir)/src/libsolus \
+	-I $(top_srcdir)/src/libsolus-ui
 
 GTK_VERSIONING_FLAGS =						\
         -DGDK_VERSION_MAX_ALLOWED=GDK_VERSION_3_20              \
