a2dbb50e2d7174245d6f034e62d66bef49732e0a
Ikey Doherty <ikey@solus-project.com>
2016-10-23 04:06:52 +0100

    panel: Begin abstraction with BudgiePanelX11Window
    Signed-off-by: Ikey Doherty <ikey@solus-project.com>

---
+++ src/panel/x11/panel-window-x11.c
@@ -0,0 +1,75 @@
+/*
+ * This file is part of budgie-next
+ *
+ * Copyright © 2016 Ikey Doherty <ikey@solus-project.com>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ */
+
+#define _GNU_SOURCE
+
+#include "util.h"
+
+SOLUS_BEGIN_PEDANTIC
+#include "panel-window-x11.h"
+SOLUS_END_PEDANTIC
+
+struct _BudgiePanelX11WindowPrivate {
+        int __reserved1;
+};
+
+G_DEFINE_TYPE_WITH_PRIVATE(BudgiePanelX11Window, budgie_panel_x11_window, BUDGIE_TYPE_PANEL_WINDOW)
+
+/**
+ * budgie_panel_x11_window_new:
+ *
+ * Construct a new BudgiePanelX11Window widget
+ */
+GtkWidget *budgie_panel_x11_window_new()
+{
+        return g_object_new(BUDGIE_TYPE_PANEL_X11_WINDOW, NULL);
+}
+
+/**
+ * Handle cleanup
+ */
+static void budgie_panel_x11_window_dispose(__solus_unused__ GObject *obj)
+{
+        /* skip for now */
+}
+
+/**
+ * Class initialisation
+ */
+static void budgie_panel_x11_window_class_init(BudgiePanelX11WindowClass *klazz)
+{
+        GObjectClass *obj_class = G_OBJECT_CLASS(klazz);
+
+        /* gobject vtable hookup */
+        obj_class->dispose = budgie_panel_x11_window_dispose;
+}
+
+/**
+ * Instaniation
+ */
+static void budgie_panel_x11_window_init(BudgiePanelX11Window *self)
+{
+        self->priv = budgie_panel_x11_window_get_instance_private(self);
+}
+
+
+/*
+ * Editor modelines  -  https://www.wireshark.org/tools/modelines.html
+ *
+ * Local variables:
+ * c-basic-offset: 8
+ * tab-width: 8
+ * indent-tabs-mode: nil
+ * End:
+ *
+ * vi: set shiftwidth=8 tabstop=8 expandtab:
+ * :indentSize=8:tabSize=8:noTabs=true:
+ */
---
+++ src/panel/x11/panel-window-x11.h
@@ -0,0 +1,61 @@
+/*
+ * This file is part of budgie-next
+ *
+ * Copyright © 2016 Ikey Doherty <ikey@solus-project.com>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ */
+
+#pragma once
+
+#include <gtk/gtk.h>
+#include "panel-window.h"
+
+G_BEGIN_DECLS
+
+typedef struct _BudgiePanelX11WindowPrivate BudgiePanelX11WindowPrivate;
+typedef struct _BudgiePanelX11Window BudgiePanelX11Window;
+typedef struct _BudgiePanelX11WindowClass BudgiePanelX11WindowClass;
+
+#define BUDGIE_TYPE_PANEL_X11_WINDOW budgie_panel_x11_window_get_type()
+#define BUDGIE_PANEL_X11_WINDOW(o)                                                                     \
+        (G_TYPE_CHECK_INSTANCE_CAST((o), BUDGIE_TYPE_PANEL_X11_WINDOW, BudgiePanelX11Window))
+#define BUDGIE_IS_PANEL_X11_WINDOW(o) (G_TYPE_CHECK_INSTANCE_TYPE((o), BUDGIE_TYPE_PANEL_X11_WINDOW))
+#define BUDGIE_PANEL_X11_WINDOW_CLASS(o)                                                               \
+        (G_TYPE_CHECK_CLASS_CAST((o), BUDGIE_TYPE_PANEL_X11_WINDOW, BudgiePanelX11WindowClass))
+#define BUDGIE_IS_PANEL_X11_WINDOW_CLASS(o) (G_TYPE_CHECK_CLASS_TYPE((o), BUDGIE_TYPE_PANEL_X11_WINDOW))
+#define BUDGIE_PANEL_X11_WINDOW_GET_CLASS(o)                                                           \
+        (G_TYPE_INSTANCE_GET_CLASS((o), BUDGIE_TYPE_PANEL_X11_WINDOW, BudgiePanelX11WindowClass))
+
+
+struct _BudgiePanelX11WindowClass {
+        BudgiePanelWindowClass parent_class;
+};
+
+struct _BudgiePanelX11Window {
+        BudgiePanelWindow parent;
+        BudgiePanelX11WindowPrivate *priv;
+};
+
+GtkWidget *budgie_panel_x11_window_new(void);
+
+GType budgie_panel_x11_window_get_type(void);
+
+
+G_END_DECLS
+
+/*
+ * Editor modelines  -  https://www.wireshark.org/tools/modelines.html
+ *
+ * Local variables:
+ * c-basic-offset: 8
+ * tab-width: 8
+ * indent-tabs-mode: nil
+ * End:
+ *
+ * vi: set shiftwidth=8 tabstop=8 expandtab:
+ * :indentSize=8:tabSize=8:noTabs=true:
+ */
