653118a58245a68716b6f1245af5428cd622749c
Ikey Doherty <ikey@solus-project.com>
2016-10-28 05:00:19 +0100

    panel: Fix licensing
    Signed-off-by: Ikey Doherty <ikey@solus-project.com>

--- src/panel/panel-window-private.h
+++ src/panel/panel-window-private.h
@@ -3,10 +3,10 @@
  *
  * Copyright © 2016 Ikey Doherty <ikey@solus-project.com>
  *
- * This library is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public
- * License as published by the Free Software Foundation; either
- * version 2.1 of the License, or (at your option) any later version.
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
  */
 
 #pragma once
